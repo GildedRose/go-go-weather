966acac03d017e3fef90b119bb87938c969b9a73
Anne <anne.k.plummer@gmail.com>
2020-06-29 00:40:03 -0500

    renamed js file

---
+++ assets/js/home.js
@@ -0,0 +1,9 @@
+var userFormEl = document.querySelector("user-form");
+var nameInputEl = document.querySelector("username");
+
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+};
+
