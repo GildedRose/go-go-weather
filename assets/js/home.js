2ae0e91c40998b1aca2ab624a761e1569981aa7b
Anne <anne.k.plummer@gmail.com>
2020-06-30 01:06:26 -0500

    response from OpenWeather passed through getWeather

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,56 @@
-var userFormEl = document.querySelector("user-form");
-var nameInputEl = document.querySelector("username");
+var userFormEl = document.querySelector("#user-form");
+var searchEl = document.querySelector("#search");
+var currentWeatherEl = document.querySelector("#current-container");
 
+var i;
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
     console.log(event);
 };
 
+var getWeather = function(zipCode) {
+    var openWeather =  'https://api.openweathermap.org/data/2.5/weather?zip=' +
+        "78251" +
+    '&appid=b1297dbea07dac5052c9756cbdb9040d'
+    fetch(openWeather)       
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayCurrent(data);
+                });
+        } else {
+            alert("Error:" + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to Weather");
+    });
+    };
+
+var displayCurrent = function(weather) {
+    currentWeatherEl.textContent = "";
+    //searchEl.textContent = zip;
+
+    //check if api returend results
+    if (weather.length === 0) {
+        currentWeatherEl.textContent = "No weather found.";
+        }
+}
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var zipCode = searchEl.value.trim();
+
+    if(zipCode) {
+        getWeather(zipCode);
+        searchEl.value = "";
+    }
+}
+
+
+userFormEl.addEventListener("submit", formSubmitHandler)
+
+
+// array needed
+// response.weather[i].description 
---
+++ script.js
@@ -0,0 +1,12 @@
+var hello = function() {
+    console.log("inside hello function")
+};
+var test = function() {
+    console.log("insde test function");
+    console.log(param1);
+};
+var goodbye = function(param1) {
+    console.log("goodbye function");
+    console.log(param1);
+
+};
